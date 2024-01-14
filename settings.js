103e487d0affe86d69c135257f2aba0c41a91f5d
hacker086 <109326216+hacker086@users.noreply.github.com>
2024-01-14 20:19:53 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -21,19 +21,19 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['916909137213'] //ur owner number
-global.ownernomer = "916909137213" //ur owner number2
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
+global.owner = ['923406902086'] //ur owner number
+global.ownernomer = "923406902086" //ur owner number2
+global.ownername = "°•𓆩𝑹𝑬𝑯𝑴𝑨𝑵𓆪•°" //ur owner name
 global.ytname = "YT: Xeon" //ur yt chanel name
 global.socialm = "GitHub: DGXeon" //ur github or insta name
 global.location = "India, Mizoram, Aizawl" //ur location
 
 //new
-global.botname = "Cheems Bot MD V8"
-global.ownernumber = '916909137213'
-global.ownername = '🦄드림 가이 Xeon'
-global.ownerNumber = ["916909137213@s.whatsapp.net"]
-global.ownerweb = "https://youtube.com/@DGXeon"
+global.botname = "°•𓆩𝑹𝑬𝑯𝑴𝑨𝑵𓆪•°"
+global.ownernumber = '923406902086'
+global.ownername = '°•𓆩𝑹𝑬𝑯𝑴𝑨𝑵𓆪•°'
+global.ownerNumber = ["923406902086@s.whatsapp.net"]
+global.ownerweb = ""
 global.websitex = "https://youtu.be/L_SIk59QeAU"
 global.wagc = "https://chat.whatsapp.com/Dc2qyVeK8JbJq8Gr3U1pKH"
 global.themeemoji = '🪀'
